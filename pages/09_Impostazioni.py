3623cd846f1ca8e0cfa840e05842e02dad8ce89d
Henluar1 <ernesto.giannoccaro@gmail.com>
2026-05-04 13:00:15 +0200

    Update and rename 09_Configurazione.py to 09_Impostazioni.py

--- pages/09_Impostazioni.py
+++ pages/09_Impostazioni.py
@@ -2,9 +2,6 @@ import streamlit as st
 from motore import styles
 import motore.ui_components as ui
 
-# --- SETUP PAGINA ---
-st.set_page_config(page_title="Configurazione", layout="wide", page_icon="⚙️")
-
 # --- CONTROLLO ACCESSO ---
 if not st.session_state.get("password_correct", False):
     st.warning("Esegui prima il login nella Dashboard principale.")
